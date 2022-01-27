3c8e7b9211f8bdadd3e9832d1a173a10f7ec3194
Peter Loft <peterloft@Peters-MacBook-Pro.local>
2022-01-26 19:45:47 -0600

    Help from Nate, dropdown not sending full query string

--- app.py
+++ app.py
@@ -9,6 +9,15 @@ app.config['SECRET_KEY'] = "secret"
 
 debug = DebugToolbarExtension(app)
 
+@app.get('/')
+def show_landing_page():
+    """ 
+    Takes prompts from the story instance, updates questions
+    html form to reflect prompts. When submitted makes a GET 
+    request with form values to the flask show_story 
+    """
+    
+    return render_template("dropdown.html", templates = ["silly", "excited"])
 
 @app.get('/form')
 def show_form():
@@ -17,8 +26,10 @@ def show_form():
     html form to reflect prompts. When submitted makes a GET 
     request with form values to the flask show_story 
     """
+    print(request.args)
+    template = silly_story.prompts if request.args["silly"] == "silly" else excited_story.prompts
     
-    return render_template("questions.html", prompts=silly_story.prompts)
+    return render_template("questions.html", prompts = template)
 
 
 @app.get('/story')
