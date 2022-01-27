b2739d650bc3160aac758cd1f314f1efb64a7dbd
Lidan-Huang <huangld0314@gmail.com>
2022-01-26 16:54:39 -0800

    started doing drop-down menu

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from click import prompt
 from flask import Flask, render_template, request
 from flask_debugtoolbar import DebugToolbarExtension
 
-from stories import silly_story
+from stories import silly_story, excited_story
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "secret"
@@ -10,14 +10,14 @@ app.config['SECRET_KEY'] = "secret"
 debug = DebugToolbarExtension(app)
 
 
-@app.get('/')
+@app.get('/form')
 def show_form():
     """ 
     Takes prompts from the story instance, updates questions
     html form to reflect prompts. When submitted makes a GET 
     request with form values to the flask show_story 
     """
-
+    
     return render_template("questions.html", prompts=silly_story.prompts)
 
 
