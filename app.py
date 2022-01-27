1803334e6a1b8da2341b59a173ef819387473b31
Lidan-Huang <huangld0314@gmail.com>
2022-01-26 16:05:09 -0800

    finished the form showing in browser

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from click import prompt
 from flask import Flask, render_template, request
 from flask_debugtoolbar import DebugToolbarExtension
 
@@ -7,3 +8,10 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = "secret"
 
 debug = DebugToolbarExtension(app)
+
+@app.get('/templates/questions.html')
+def show_form():
+    prompts = silly_story.prompts 
+    # replace prompts inside of return statement
+    
+    return render_template("questions.html", prompts = prompts)
