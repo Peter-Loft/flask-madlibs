ae0019056d1811b78d130fd4f9e4b7a44f9db33b
Peter Loft <peterloft@Peters-MacBook-Pro.local>
2022-01-26 18:25:51 -0600

    finalized docstrings and show story function.

--- app.py
+++ app.py
@@ -9,9 +9,26 @@ app.config['SECRET_KEY'] = "secret"
 
 debug = DebugToolbarExtension(app)
 
-@app.get('/templates/questions.html')
+
+@app.get('/')
 def show_form():
-    prompts = silly_story.prompts 
+    """ 
+    Takes prompts from the story instance, updates questions
+    html form to reflect prompts. When submitted makes a GET 
+    request with form values to the flask show_story 
+    """
+
+    prompts = silly_story.prompts
     # replace prompts inside of return statement
-    
-    return render_template("questions.html", prompts = prompts)
+
+    return render_template("questions.html", prompts=prompts)
+
+
+@app.get('/story')
+def show_story():
+    """ 
+    Accepts dictionary-like object from show_form GET 
+    request, calls generate method to produce story text.
+    """
+
+    return render_template("story.html", text=silly_story.generate(request.args))
